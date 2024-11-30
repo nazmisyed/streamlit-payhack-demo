a30900efda3361988f4cc542516d3e7c071ebc44
haoyee5235 <haoyee5235@gmail.com>
2024-11-30 13:13:40 +0800

    Add script to send data to Eventhub

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
+from eventhub_receiver import run
+import asyncio
 
 # Load environment variables
 load_dotenv()
@@ -10,16 +12,11 @@ st.title("Bank Transaction Demo")
 # Input fields for transaction details
 st.header("Enter Transaction Details")
 
-account_number = st.text_input("Email")
-account_number = st.text_input("Account Number")
+email = st.text_input("Email")
+account_number = st.text_input("Account Number", value = 100)
 transaction_type = st.selectbox("Transaction Type", ["Deposit", "Withdrawal"])
 amount = st.number_input("Amount", min_value=0.0, step=0.01)
 
 # Button to submit transaction
 if st.button("Submit Transaction"):
-    if transaction_type == "Deposit":
-        st.success(f"Successfully deposited ${amount} to account {account_number}.")
-    elif transaction_type == "Withdrawal":
-        st.success(f"Successfully withdrew ${amount} from account {account_number}.")
-    else:
-        st.error("Invalid transaction type.")
+    asyncio.run(run(email))
